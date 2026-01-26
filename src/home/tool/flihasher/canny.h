3e56d7826c0caaa7c21022e94b71cbccd033afd5
heimdallr <heimdalr@ngs.ru>
2026-01-26 11:48:32 +0700

    #33 Не инициализировать поля и возвращать сгенерированное дефолтным конструктором???

--- src/home/tool/flihasher/canny.h
+++ src/home/tool/flihasher/canny.h
@@ -9,7 +9,7 @@ class Canny
 public:
 	struct Rect
 	{
-		int top, left, bottom, right;
+		int top { 0 }, left { 0 }, bottom { 0 }, right { 0 };
 
 		int width() const noexcept
 		{
@@ -21,6 +21,7 @@ public:
 			return bottom - top;
 		}
 	};
+
 public:
 	Canny(int gaussianFilterSize = 3, double gaussianSigma = 1, int thresholdLow = 20, int thresholdHigh = 40);
 	Rect Process(const cimg_library::CImg<unsigned char>& img) const;
